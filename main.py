cd15352674a09795565442a9d50f957ef055d231
zhangmengdan1 <2037228815@qq.com>
2025-10-18 16:09:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
+letter_count = 0
+digit_count = 0
+space_count = 0
+other_count = 0
 
-s = input("请输入一行字符：")
-
-# 初始化各类字符计数器
-letter_count = 0  # 英文字符计数器
-digit_count = 0   # 数字计数器
-space_count = 0   # 空格计数器
-other_count = 0   # 其他字符计数器
-
-# 遍历输入的每一个字符
-for char in s:
-    if char.isalpha():  # 判断是否为英文字符（字母）
+input_str = input("请输入一行字符: ")
+for char in input_str:
+    if char.isalpha():
         letter_count += 1
-    elif char.isdigit():  # 判断是否为数字
+    elif char.isdigit():
         digit_count += 1
-    elif char.isspace():  # 判断是否为空格
+    elif char.isspace():
         space_count += 1
-    else:  # 其他字符
+    else:
         other_count += 1
 
-# 按照要求格式输出结果
-print("英文字符:", letter_count)
-print("数字:", digit_count)
-print("空格:", space_count)
-print("其他字符:", other_count)
+print(f"英文字母:{letter_count}; 数字:{digit_count}; 空格:{space_count}; 其他字符:{other_count}")
